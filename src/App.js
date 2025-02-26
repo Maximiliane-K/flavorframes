0066b0d9e69b23abb2afa3687eecdfc5c4af5647
Maximiliane <115705639+Maximiliane-K@users.noreply.github.com>
2025-02-26 21:14:46 +0100

    Add basic EventPage and implement logic to fetch data

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 import EventCreateForm from "./pages/events/EventCreateForm";
+import EventPage from "./pages/events/EventPage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -57,6 +58,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/events/create" render={() => <EventCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/events/:id" render={() => <EventPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route
